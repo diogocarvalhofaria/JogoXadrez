6b5a5d0bc1c1787a3cfa6238aa31da090f350f71
diogo.carvalho <di.c.faria@gmail.com>
2024-08-18 12:48:47 -0300

    Jogo Xadrez

---
+++ Program/src/boardgame/BoardEXception.java
@@ -0,0 +1,9 @@
+package boardgame;
+
+public class BoardEXception extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public BoardEXception(String msg){
+        super(msg);
+    }
+}
